2c450b5ea112aaff8762d65ba406972d7ded28d4
Ankit Mordhwaj <mordhwaj.ankit@gmail.com>
2023-01-04 20:23:55 +0530

    uploading media

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -122,6 +122,12 @@ STATICFILES_DIRS = (
     Path.joinpath(BASE_DIR, 'assets'),
 )
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = (
+    Path.joinpath(BASE_DIR, 'media')
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
