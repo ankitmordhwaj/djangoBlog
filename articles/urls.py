961abd7ec67d3d36a737dcd7262cf575bd6f815c
Ankit Mordhwaj <mordhwaj.ankit@gmail.com>
2023-01-04 17:34:40 +0530

    named urls

--- articles/urls.py
+++ articles/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.artice_list),
+    path('', views.artice_list, name='list'),
     # path(r'^(?P<slug>[\w]+)/$', views.artice_detail),
-    path('<slug>/', views.artice_detail),
+    path('<slug>/', views.artice_detail, name='detail'),
 ]
