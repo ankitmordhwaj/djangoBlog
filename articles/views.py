1679b932d98912c99caae415776bccd739138f6d
Ankit Mordhwaj <mordhwaj.ankit@gmail.com>
2023-01-09 18:57:20 +0530

    models form

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 # from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 def artice_list(request):
@@ -20,9 +21,12 @@ def artice_detail(request, slug):
 
 @login_required(login_url="/accounts/login")
 def artice_create(request):
-    # articles = Article.objects.all().order_by('date')
-    # article = Article.objects.get(slug = slug)
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid:
+            # save article to db
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+        return render(request, 'articles/article_create.html', {'form': form})
 
-    return render(request, 'articles/article_create.html')
-
-    # return HttpResponse(slug)
